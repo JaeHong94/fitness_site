17ce3053ddbfc5da0e5fab99d3e77a2ef2c91cea
JaeHong94 <moonshin123@naver.com>
2021-11-30 20:00:21 +0900

    ADD: home section

--- js/script.js
+++ js/script.js
@@ -4,4 +4,19 @@ const navbar = document.querySelector('.header .navbar');
 menu.addEventListener('click', () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
-})
+});
+window.addEventListener('scroll', () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+});
+
+const swiper = new Swiper('.home-slider', {
+  spaceBetween: 20,
+  effect: 'fade',
+  grabCursor: true,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
